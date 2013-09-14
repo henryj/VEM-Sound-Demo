a42279741d6e839b88a2eab5d453ce33bd59a21f
Henry J <m@jjw.in>
2013-09-15 09:32:50 +1000

    init

---
+++ other/other.lua
@@ -0,0 +1,17 @@
+-- VEM_CORE_SOUNDGRIL_CUSTOM 显示为语音包选项中你的语音包名
+-- VEM_CORE_SOUNDGRIL_NAME_CUSTOM 显示为你的语音包介绍标题
+-- VEM_CORE_SOUNDGRIL_TEXT_CUSTOM 显示为你的语音包介绍内容
+
+if GetLocale() == "zhTW" then
+	VEM_CORE_SOUNDGRIL_CUSTOM = "VoiceRss(EN)"
+	VEM_CORE_SOUNDGRIL_NAME_CUSTOM = "VoiceRss"
+	VEM_CORE_SOUNDGRIL_TEXT_CUSTOM = "    All Voice generated via VoiceRss TTS (Text To Speech) API by Iceoven (Worgen Mage from Ancient Ones @ US-Dath'Remar)."
+elseif  GetLocale() == "zhCN" then
+	VEM_CORE_SOUNDGRIL_CUSTOM = "VoiceRss(EN)"
+	VEM_CORE_SOUNDGRIL_NAME_CUSTOM = "VoiceRss"
+	VEM_CORE_SOUNDGRIL_TEXT_CUSTOM = "    All Voice generated via VoiceRss TTS (Text To Speech) API by Iceoven (Worgen Mage from Ancient Ones @ US-Dath'Remar)."
+else
+	VEM_CORE_SOUNDGRIL_CUSTOM = "VoiceRss(EN)"
+	VEM_CORE_SOUNDGRIL_NAME_CUSTOM = "VoiceRss"
+	VEM_CORE_SOUNDGRIL_TEXT_CUSTOM = "    All Voice generated via VoiceRss TTS (Text To Speech) API by Iceoven (Worgen Mage from Ancient Ones @ US-Dath'Remar)."
+end
